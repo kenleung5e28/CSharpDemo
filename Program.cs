6885fab89d7962d6c193039b11bf7e2c4570f6b8
Ken C.Y. Leung <kenleung5e28@gmail.com>
2022-10-09 10:20:25 +0000

    update

--- Program.cs
+++ Program.cs
@@ -1,6 +1,14 @@
 ﻿using System.Threading;
 using System.Threading.Tasks;
 
-Console.WriteLine("Before delay: {0}", Thread.CurrentThread.ManagedThreadId);
-await Task.Delay(100);
-Console.WriteLine("After delay: {0}", Thread.CurrentThread.ManagedThreadId);
+static async Task f(string id, int delay) {
+    Console.WriteLine("Before {0}: {1}", id, Thread.CurrentThread.ManagedThreadId);
+    await Task.Delay(delay);
+    Console.WriteLine("After {0}: {1}", id, Thread.CurrentThread.ManagedThreadId);
+}
+
+Task t1 = f("first", 2000);
+Task t2 = f("second", 500);
+Console.WriteLine("Before WhenAll: {0}", Thread.CurrentThread.ManagedThreadId);
+await Task.WhenAny(t1, t2);
+Console.WriteLine("After WhenAll: {0}", Thread.CurrentThread.ManagedThreadId);
